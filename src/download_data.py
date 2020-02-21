f86109b210a7850215bb247010af01cf75c2cdbd
Russell Gould <gouldru@oregonstate.edu>
2020-02-21 12:35:06 -0800

    check if folder exists

--- src/download_data.py
+++ src/download_data.py
@@ -1,10 +1,15 @@
 from os import system
 from pathlib import Path
 
+raw_peaks = Path("../data/raw_peaks").resolve()
+
+if not raw_peaks.exists():
+    raw_peaks.mkdir()
+
 with open("../data/peak_file_list.txt", "r") as f:
     for line in f:
         line = line.strip()
         uri = f"http://hgdownload.soe.ucsc.edu/goldenPath/hg19/encodeDCC/{line}.gz"
-        path = Path(f"../data/raw_peaks/{line.split('/')[1]}.gz").resolve()
+        path = raw_peaks / f"{line.split('/')[1]}.gz"
         system(f"wget {uri} -O {path}")
         system(f"gzip -d {path}")
